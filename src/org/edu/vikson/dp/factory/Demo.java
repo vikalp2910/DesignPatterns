63236cf8b4a6f7b6c6b9b6cca161e977b64317dc
vikalp2910 <vikalp2910@gmail.com>
2023-08-26 10:27:42 +0530

    dp

---
+++ src/org/edu/vikson/dp/factory/Demo.java
@@ -0,0 +1,88 @@
+package org.edu.vikson.dp.factory;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+
+
+
+public  class Demo   {
+   
+	   public static boolean balancedParenthesis(String inputStr) {   
+	          
+	        // create an empty stack   
+	        Stack stack = new Stack();   
+	          
+	        // convert inputStr to char array   
+	        char[] charArray = inputStr.toCharArray();   
+	          
+	        // iterate charArray   
+	        for (int i = 0; i < charArray.length; i++) {   
+	              
+	            // get char   
+	            char current = charArray[i];   
+	              
+	            //check whether current is '(', '[' or '{'   
+	            if (current == '{' || current == '[' || current == '(') {   
+	                // push current to stack   
+	                stack.push(current);   
+	                continue;   
+	            }   
+	            // if stack is empty, return false   
+	            if (stack.isEmpty()) {    
+	                return false;   
+	            }   
+	              
+	            // use switch statement to pop element from stack and if it is '(', '[' or '{', return false   
+	            char popChar;   
+	            switch (current) {   
+	                case ')':   
+	                popChar = (char) stack.pop();   
+	                if (popChar == '{' || popChar == '[')   
+	                    return false;   
+	                break;   
+	                case '}':   
+	                popChar = (char) stack.pop();   
+	                if (popChar == '(' || popChar == '[')   
+	                    return false;   
+	                break;   
+	                case ']':   
+	                popChar = (char) stack.pop();   
+	                if (popChar == '(' || popChar == '{')   
+	                    return false;   
+	                break;   
+	            }   
+	        }   
+	        return (stack.isEmpty());   
+	    }   
+	   
+	   
+	public static void main(String[] args) {
+		
+		  String inputStr;   
+          
+	        // create an instance of Scanner class   
+	        Scanner sc = new Scanner(System.in);   
+	          
+	       // System.out.println("Enter input string to check:");   
+	        // take input sring from user   
+	       // inputStr = sc.nextLine();   
+	          inputStr="(";
+	          
+	        // call balancedParenthesis() method to check whether input string is balanced of not   
+	        if (balancedParenthesis(inputStr))   
+	            System.out.println("Input string "+inputStr+" is balanced.");   
+	        else   
+	            System.out.println("Input string "+inputStr+" is not balanced.");   
+		
+		
+		
+		
+		
+	}
+  }
+	
+	
+	
+	
+	
